6ea3e0cf1f2355b7ef00b72b552ad12cb01a82a6
Fara <115084566+KHALMATOV-FARRUKH@users.noreply.github.com>
2022-11-19 19:08:38 +0500

    Created folders: models screens widgets
    Added dependency:
    equatable: ^2.0.5
    
    Created dart files:
    article_model.dart
    article_screen.dart
    discover_screen.dart
    home_screen.dart
    screens.dart
    bottom_nav_bar.dart
    custom_tag.dart
    image_container.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'screens/screens.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter News App UI',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      initialRoute: '/',
+      routes: {
+        HomeScreen.routeName: (context) => const HomeScreen(),
+        DiscoverScreen.routeName: (context) => const DiscoverScreen(),
+        ArticleScreen.routeName: (context) => const ArticleScreen(),
+      },
     );
   }
 }
